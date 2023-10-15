e59d91d71d80b7beee3b84a78930f3506d38c51b
Pacelin <Warfacer2020@gmail.com>
2023-10-15 22:22:50 +0300

    feat: Player Step Simulation

---
+++ Assets/Scripts/Player/PlayerStepSimulation.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerStepSimulation : MonoBehaviour
+{
+	public UnityEvent OnStep = new UnityEvent();
+	[SerializeField] private Transform _player;
+	[SerializeField] private float _stepLength;
+
+	private float _currentLength;
+	private Vector3 _lastPosition;
+
+	private void Awake()
+	{
+		_currentLength = 0;
+		_lastPosition = _player.position;
+	}
+
+	private void Update()
+	{
+		_currentLength += Vector3.Distance(_lastPosition, _player.position);
+		_lastPosition = _player.position;
+
+		if (_currentLength >= _stepLength)
+		{
+			_currentLength = 0;
+			OnStep.Invoke();
+		}
+	}
+}
